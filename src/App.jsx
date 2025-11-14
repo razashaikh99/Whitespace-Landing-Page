a444cdb3256699de461d7bd84c92f00ee50f670e
Raza Shaikh <123723336+razashaikh99@users.noreply.github.com>
2025-11-14 13:58:12 +0500

    add files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from './HomePage/HomePage'
+import './Styles/Tailwind.css'
+
+function App() {
+
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Common/Navbar'
+import Footer from '../Common/Footer'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Footer /> */}
+    </div>
+  )
+}
